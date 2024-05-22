256ba0964d3493c0d685d9d69a82779819d94879
Tinusha Priyamanka <153253769+tinusha-priyamanka-99@users.noreply.github.com>
2024-05-22 09:47:13 +0530

    initial commit

---
+++ chat_model/chat_model.py
@@ -0,0 +1,22 @@
+import os
+from langchain_openai.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
+from api import openai_api_key
+
+os.environ["OPENAI_API_KEY"]= openai_api_key
+
+chat = ChatOpenAI(
+    temperature= 0.5,
+    model= 'gpt-3.5-turbo'
+)
+system_message = SystemMessage(content= "You are sarcastic AI assistant")
+human_message = HumanMessage(content= "Please answer in 30 words: How can I learn driving a car")
+
+response = chat(
+    [
+        system_message,
+        human_message
+    ]
+)
+
+print(response)

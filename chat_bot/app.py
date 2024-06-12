5129a97ed8a767affc8fbbe285423840ea6d5171
Tinusha Priyamanka <153253769+tinusha-priyamanka-99@users.noreply.github.com>
2024-06-13 01:32:28 +0530

    chat bot for a web site

--- chat_bot/app.py
+++ chat_bot/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from utils import get_website_data, split_data, create_embeddings
+from utils import *
+import constants
 
 st.title("AI Assistance For Website")
 
@@ -15,17 +16,55 @@ load_button = st.sidebar.button("Load the data to Pinecone",
 if load_button:
 
     if st.session_state['HuggingFace_API_Key'] != "" and st.session_state['Pinecone_API_Key'] != "":
-        
-        site_data= get_website_data("https://www.langchain.com/sitemap.xml")
+         
+        site_data= get_website_data(constants.WEBSITE_URL)
         st.write("Data pull done...")
         
         chunks_data = split_data(site_data)
         st.write("Splitting data done...")
         
-        embeddings = create_embeddings(chunks_data)
+        embeddings = create_embeddings()
         st.write("Embeddings instance creation done...")
+
+        push_to_pinecone(st.session_state['Pinecone_API_Key'],
+                         constants.PINECONE_ENVIRONMENT,
+                         constants.PINECONE_INDEX,
+                         embeddings,
+                         chunks_data)
         st.write("Pushing data to Pinecone done...")
         st.sidebar.write("Data pushed to Pinecone successfully!")
 
-    else:
+
+
+prompt = st.text_input('How can I help you?', key='prompt')
+document_count = st.slider('No of links to return - (0 LOW || 5 HIGH)',0,5,2,step=1)
+
+submit = st.button('Search')
+
+if submit:
+        
+        if st.session_state['HuggingFace_API_Key'] != "" and st.session_state['Pinecone_API_Key'] != "":
+             
+             embeddings = create_embeddings()
+             st.write("Embeddings instance creation done...")
+
+             index = pull_from_pinecone(st.session_state['Pinecone_API_Key'],
+                         constants.PINECONE_ENVIRONMENT,
+                         constants.PINECONE_INDEX,
+                         embeddings)
+             st.write("Pinecone index retrieval done...")
+
+             relavant_docs = get_similar_docs(index,prompt,document_count)   
+             st.write(relavant_docs)
+
+             st.success('Please find the search results :')
+
+
+             for document in relavant_docs:
+                  
+                  st.write("Results : "+ str(relavant_docs.index(document)+1)+ "**")
+                  st.write("**Info**: "+document.page_content)
+                  st.write("**Link**: "+ document.metadata['source'])
+
+else:
         st.sidebar.error("Please provide API keys...")
--- chat_bot/utils.py
+++ chat_bot/utils.py
@@ -4,6 +4,9 @@ from langchain_community.embeddings import SentenceTransformerEmbeddings
 import pinecone
 import asyncio
 from langchain_community.document_loaders import SitemapLoader
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def get_website_data(sitemap_url):
 
@@ -29,3 +32,36 @@ def split_data(docs):
 def create_embeddings():
     embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
     return embeddings
+
+def push_to_pinecone(pinecone_apikey,
+                     pinecone_environment,
+                     pinecone_index_name,
+                     embeddings,
+                     docs):
+    
+    pc = pinecone.Pinecone(
+    api_key=os.environ.get("PINECONE_API_KEY"),
+    environment= pinecone_environment
+    )
+
+    index_name= pinecone_index_name
+    index = Pinecone.from_documents(docs,embeddings,index_name=index_name)
+    return index
+
+def pull_from_pinecone(pinecone_apikey,
+                     pinecone_environment,
+                     pinecone_index_name,
+                     embeddings):
+    
+    pc = pinecone.Pinecone(
+    api_key=os.environ.get("PINECONE_API_KEY"),
+    environment= pinecone_environment
+    )
+
+    index_name= pinecone_index_name
+    index = Pinecone.from_existing_index(index_name,embeddings)
+    return index
+
+def get_similar_docs(index,query,k=2):
+    similar_docs = index.similarity_search(query,k=k)
+    return similar_docs

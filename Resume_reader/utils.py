9498131e05a8400d9f153c1ed05e3405fdb1c8df
Tinusha Priyamanka <153253769+tinusha-priyamanka-99@users.noreply.github.com>
2024-06-13 01:36:48 +0530

    resume analyser

--- Resume_reader/utils.py
+++ Resume_reader/utils.py
@@ -6,21 +6,24 @@ from langchain.schema import Document
 import pinecone
 from pypdf import PdfReader
 from langchain.chains.summarize import load_summarize_chain
-from langchain_community.llms import HuggingFaceHub
+from langchain_community.llms.huggingface_hub import HuggingFaceHub
+from langchain_community.vectorstores.pinecone import Pinecone
+import os
 
-def get_pdf_text(pdf):
+
+def get_pdf_text(pdf_doc):
     text = ""
     pdf_reader = PdfReader(pdf_doc)
     for page in pdf_reader.pages:
         text += page.extract_text()
     return text
 
-def creatw_docs(user_pdf_list, unique_id):
+def create_docs(user_pdf_list, unique_id):
     docs=[]
     for filename in user_pdf_list:
         chunks = get_pdf_text(filename)
 
-        docs.append(document(
+        docs.append(Document(
             page_content = chunks,
             metadata={"name": filename.name,
                       "id": filename.id,
@@ -29,4 +32,62 @@ def creatw_docs(user_pdf_list, unique_id):
                       "unique_id":unique_id},
         ))
     return docs
+
+def create_embeddings_load_data():
+    embeddings = SentenceTransformerEmbeddings()
+    return embeddings
+
+def push_to_pinecone(pinecone_apikey,
+                     pinecone_environment,
+                     pinecone_index_name,
+                     embeddings,
+                     docs):
+    pc = pinecone.Pinecone(
+        api_key=os.environ.get("PINECONE_API_KEY"),
+        environment= pinecone_environment
+        )
+
+    index_name= pinecone_index_name
+    index = Pinecone.from_documents(docs,embeddings,index_name=index_name)
     
+
+def pull_from_pinecone(pinecone_apikey,
+                     pinecone_environment,
+                     pinecone_index_name,
+                     embeddings):
+    pc = pinecone.Pinecone(
+        api_key=os.environ.get("PINECONE_API_KEY"),
+        environment= pinecone_environment
+        )
+
+    index_name= pinecone_index_name
+    index = Pinecone.from_existing_index(index_name,embeddings)
+    return index
+
+def similar_docs(query,
+                 k,
+                 pinecone_apikey,
+                 pinecone_environment,
+                 pinecone_index_name,
+                 embeddings,
+                 unique_id):
+    
+
+    index_name= pinecone_index_name
+
+    index = pull_from_pinecone(pinecone_apikey,
+                               pinecone_environment,
+                               index_name,
+                               embeddings)
+    similar_docs = index.similarity_search_with_score(query, 
+                                                      int(k),
+                                                      {"unique_id":unique_id})
+    return similar_docs
+
+def get_summary(current_doc):
+    llm = OpenAI(temperature=0)
+
+    chain = load_summarize_chain(llm, chain_type="map_reduce")
+    summary = chain.run([current_doc])
+
+    return summary
